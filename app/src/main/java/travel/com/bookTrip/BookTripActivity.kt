14588a40b3d1f49b1649a5bd937c433cf93fba15
mostafa_anter <ga30000@gmail.com>
2018-09-08 13:38:55 +0200

    8/8/2018, 10:40 AM

--- app/src/main/java/travel/com/bookTrip/BookTripActivity.kt
+++ app/src/main/java/travel/com/bookTrip/BookTripActivity.kt
@@ -1,8 +1,10 @@
 package travel.com.bookTrip
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_book_trip.*
@@ -19,13 +21,33 @@ import travel.com.touristesTripResults.models.DataItem
 import travel.com.utility.SweetDialogHelper
 
 
-class BookTripActivity : AppCompatActivity() {
+class BookTripActivity : AppCompatActivity(), View.OnClickListener {
+
+    var bigNumbersList: MutableList<String> = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+    var smallNumbersList: MutableList<String> = mutableListOf("1", "2", "3", "4", "5")
+    var childrenAges: MutableList<String> = mutableListOf("1", "2")
+    var childrenAgesText: MutableList<String> = mutableListOf("من 0 الى 5 سنوات", "من 6 الى 11 سنة")
+    var roomsTypes: MutableList<String> = mutableListOf("1", "2", "3")
+    var roomsTypesText: MutableList<String> = mutableListOf("فردية", "مزدوجة", "ثلاثية")
+
+
 
     private lateinit var tripItem: DataItem
 
     private var apiService: ApiInterface? = null
     private var subscription1: Subscription? = null
     private lateinit var sweetDialogHelper: SweetDialogHelper
+
+    // for make reservation request
+    var userAgent: String? = null
+    var trip_id: String? = null
+    var adult_count: String? = null
+    var child_count: String? = null
+    var room_count: String? = null
+    var room_types = ArrayList<String>()
+    var child_ages = ArrayList<String>()
+    var payment_method: String? = null
+    var note: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +62,322 @@ class BookTripActivity : AppCompatActivity() {
         apiService = ApiClient.getClient()!!.create(ApiInterface::class.java)
         bindView()
 
+        adults_num_picker.setOnClickListener(this)
+        child_count_picker.setOnClickListener(this)
+        room_count_picker.setOnClickListener(this)
+        child1_age.setOnClickListener(this)
+        child2_age.setOnClickListener(this)
+        child3_age.setOnClickListener(this)
+        child4_age.setOnClickListener(this)
+        child5_age.setOnClickListener(this)
+        room1_type.setOnClickListener(this)
+        room2_type.setOnClickListener(this)
+        room3_type.setOnClickListener(this)
+        room4_type.setOnClickListener(this)
+        room5_type.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            adults_num_picker-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("عدد الاشخاص البالغين")
+                        .setSingleChoiceItems(bigNumbersList.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text9.text = bigNumbersList[which]
+                            adult_count = bigNumbersList[which]
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            child_count_picker-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("عدد الأطفال")
+                        .setSingleChoiceItems(smallNumbersList.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text11.text = smallNumbersList[which]
+                            child_count = smallNumbersList[which]
+
+                            when(child_count!!.toInt()){
+                                1 ->{
+                                    child_ages.clear()
+                                    text13.text = "سن الطفل 1"
+                                    child1_age.visibility = View.VISIBLE
+                                    child2_age.visibility = View.GONE
+                                    child3_age.visibility = View.GONE
+                                    child4_age.visibility = View.GONE
+                                    child5_age.visibility = View.GONE
+                                }
+                                2 ->{
+                                    child_ages.clear()
+                                    text13.text = "سن الطفل 1"
+                                    text15.text = "سن الطفل 2"
+                                    child1_age.visibility = View.VISIBLE
+                                    child2_age.visibility = View.VISIBLE
+                                    child3_age.visibility = View.GONE
+                                    child4_age.visibility = View.GONE
+                                    child5_age.visibility = View.GONE
+                                }
+                                3 ->{
+                                    child_ages.clear()
+                                    text13.text = "سن الطفل 1"
+                                    text15.text = "سن الطفل 2"
+                                    text17.text = "سن الطفل 3"
+                                    child1_age.visibility = View.VISIBLE
+                                    child2_age.visibility = View.VISIBLE
+                                    child3_age.visibility = View.VISIBLE
+                                    child4_age.visibility = View.GONE
+                                    child5_age.visibility = View.GONE
+                                }
+                                4 ->{
+                                    child_ages.clear()
+                                    text13.text = "سن الطفل 1"
+                                    text15.text = "سن الطفل 2"
+                                    text17.text = "سن الطفل 3"
+                                    text19.text = "سن الطفل 4"
+                                    child1_age.visibility = View.VISIBLE
+                                    child2_age.visibility = View.VISIBLE
+                                    child3_age.visibility = View.VISIBLE
+                                    child4_age.visibility = View.VISIBLE
+                                    child5_age.visibility = View.GONE
+                                }
+                                5 ->{
+                                    child_ages.clear()
+                                    text13.text = "سن الطفل 1"
+                                    text15.text = "سن الطفل 2"
+                                    text17.text = "سن الطفل 3"
+                                    text19.text = "سن الطفل 4"
+                                    text21.text = "سن الطفل 5"
+                                    child1_age.visibility = View.VISIBLE
+                                    child2_age.visibility = View.VISIBLE
+                                    child3_age.visibility = View.VISIBLE
+                                    child4_age.visibility = View.VISIBLE
+                                    child5_age.visibility = View.VISIBLE
+                                }
+                                else ->{
+
+                                }
+                            }
+
+
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            room_count_picker-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("عدد الغرف")
+                        .setSingleChoiceItems(smallNumbersList.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text23.text = smallNumbersList[which]
+                            room_count = smallNumbersList[which]
+
+                            when(room_count!!.toInt()){
+                                1 ->{
+                                    room_types.clear()
+                                    text25.text = "نوع الغرفة"
+                                    room1_type.visibility = View.VISIBLE
+                                    room2_type.visibility = View.GONE
+                                    room3_type.visibility = View.GONE
+                                    room4_type.visibility = View.GONE
+                                    room5_type.visibility = View.GONE
+                                }
+                                2 ->{
+                                    room_types.clear()
+                                    text25.text = "نوع الغرفة"
+                                    text27.text = "نوع الغرفة"
+                                    room1_type.visibility = View.VISIBLE
+                                    room2_type.visibility = View.VISIBLE
+                                    room3_type.visibility = View.GONE
+                                    room4_type.visibility = View.GONE
+                                    room5_type.visibility = View.GONE
+                                }
+                                3 ->{
+                                    room_types.clear()
+                                    text25.text = "نوع الغرفة"
+                                    text27.text = "نوع الغرفة"
+                                    text29.text = "نوع الغرفة"
+                                    room1_type.visibility = View.VISIBLE
+                                    room2_type.visibility = View.VISIBLE
+                                    room3_type.visibility = View.VISIBLE
+                                    room4_type.visibility = View.GONE
+                                    room5_type.visibility = View.GONE
+                                }
+                                4 ->{
+                                    room_types.clear()
+                                    text25.text = "نوع الغرفة"
+                                    text27.text = "نوع الغرفة"
+                                    text29.text = "نوع الغرفة"
+                                    text31.text = "نوع الغرفة"
+                                    room1_type.visibility = View.VISIBLE
+                                    room2_type.visibility = View.VISIBLE
+                                    room3_type.visibility = View.VISIBLE
+                                    room4_type.visibility = View.VISIBLE
+                                    room5_type.visibility = View.GONE
+                                }
+                                5 ->{
+                                    room_types.clear()
+                                    text25.text = "نوع الغرفة"
+                                    text27.text = "نوع الغرفة"
+                                    text29.text = "نوع الغرفة"
+                                    text31.text = "نوع الغرفة"
+                                    text33.text = "نوع الغرفة"
+                                    room1_type.visibility = View.VISIBLE
+                                    room2_type.visibility = View.VISIBLE
+                                    room3_type.visibility = View.VISIBLE
+                                    room4_type.visibility = View.VISIBLE
+                                    room5_type.visibility = View.VISIBLE
+                                }
+                                else ->{
+
+                                }
+                            }
+
+
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+
+            child1_age-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("سن الطفل")
+                        .setSingleChoiceItems(childrenAgesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text13.text = childrenAgesText[which]
+                            child_ages.add(childrenAges[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            child2_age-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("سن الطفل")
+                        .setSingleChoiceItems(childrenAgesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text15.text = childrenAgesText[which]
+                            child_ages.add(childrenAges[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            child3_age-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("سن الطفل")
+                        .setSingleChoiceItems(childrenAgesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text17.text = childrenAgesText[which]
+                            child_ages.add(childrenAges[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            child4_age-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("سن الطفل")
+                        .setSingleChoiceItems(childrenAgesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text19.text = childrenAgesText[which]
+                            child_ages.add(childrenAges[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            child5_age-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("سن الطفل")
+                        .setSingleChoiceItems(childrenAgesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text21.text = childrenAgesText[which]
+                            child_ages.add(childrenAges[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+
+            room1_type-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("نوع الغرفة")
+                        .setSingleChoiceItems(roomsTypesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text25.text = roomsTypesText[which]
+                            room_types.add(roomsTypes[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            room2_type-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("نوع الغرفة")
+                        .setSingleChoiceItems(roomsTypesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text27.text = roomsTypesText[which]
+                            room_types.add(roomsTypes[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            room3_type-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("نوع الغرفة")
+                        .setSingleChoiceItems(roomsTypesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text29.text = roomsTypesText[which]
+                            room_types.add(roomsTypes[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            room4_type-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("نوع الغرفة")
+                        .setSingleChoiceItems(roomsTypesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text31.text = roomsTypesText[which]
+                            room_types.add(roomsTypes[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+            room5_type-> {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("نوع الغرفة")
+                        .setSingleChoiceItems(roomsTypesText.toTypedArray(), 0, DialogInterface.OnClickListener { dialog, which ->
+                            // reset cities
+                            text33.text = roomsTypesText[which]
+                            room_types.add(roomsTypes[which])
+                        })
+                builder.setPositiveButton("Okey") { dialog, which ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+            }
+        }
     }
 
     fun changeViewsFonts() {
@@ -68,22 +406,22 @@ class BookTripActivity : AppCompatActivity() {
         when (view.getId()) {
             R.id.radio1 -> {
                 if (checked){
-
+                    payment_method = "1"
                 }
             }
             R.id.radio2 -> {
                 if (checked){
-
+                    payment_method = "2"
                 }
             }
             R.id.radio3 -> {
                 if (checked){
-
+                    payment_method = "3"
                 }
             }
             R.id.radio4 -> {
                 if (checked){
-
+                    payment_method = "4"
                 }
             }
 
@@ -108,6 +446,19 @@ class BookTripActivity : AppCompatActivity() {
 
             ratingValue.text =  stars.toFloat().toString()
             text6.text = "from $start_date to $end_date"
+
+            // hide child age and room types
+            child1_age.visibility = View.GONE
+            child2_age.visibility = View.GONE
+            child3_age.visibility = View.GONE
+            child4_age.visibility = View.GONE
+            child5_age.visibility = View.GONE
+
+            room1_type.visibility = View.GONE
+            room2_type.visibility = View.GONE
+            room3_type.visibility = View.GONE
+            room4_type.visibility = View.GONE
+            room5_type.visibility = View.GONE
         }
     }
 
